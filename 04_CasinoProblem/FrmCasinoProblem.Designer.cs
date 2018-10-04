29bc6a774615da91586667849a94075385f79740
jmubago <jmubagosotomayor@gmail.com>
2018-10-04 20:58:37 +0200

    Agregar archivos de proyecto.

---
+++ 04_CasinoProblem/FrmCasinoProblem.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _04_CasinoProblem
+{
+    partial class FrmCasinoProblem
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnPlay = new System.Windows.Forms.Button();
+            this.LstView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // BtnPlay
+            // 
+            this.BtnPlay.Location = new System.Drawing.Point(165, 47);
+            this.BtnPlay.Name = "BtnPlay";
+            this.BtnPlay.Size = new System.Drawing.Size(175, 27);
+            this.BtnPlay.TabIndex = 0;
+            this.BtnPlay.Text = "Play";
+            this.BtnPlay.UseVisualStyleBackColor = true;
+            this.BtnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
+            // LstView
+            // 
+            this.LstView.Location = new System.Drawing.Point(54, 92);
+            this.LstView.Name = "LstView";
+            this.LstView.Size = new System.Drawing.Size(393, 191);
+            this.LstView.TabIndex = 1;
+            this.LstView.UseCompatibleStateImageBehavior = false;
+            // 
+            // FrmCasinoProblem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 308);
+            this.Controls.Add(this.LstView);
+            this.Controls.Add(this.BtnPlay);
+            this.Name = "FrmCasinoProblem";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnPlay;
+        private System.Windows.Forms.ListView LstView;
+    }
+}
+
---
+++ 04_CasinoProblem/FrmCasinoProblem.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_CasinoProblem
+{
+    public partial class FrmCasinoProblem : Form
+    {
+        public FrmCasinoProblem()
+        {
+            InitializeComponent();
+        }
+
+
+        //At a casino, the following game is played between a player and the banker.The player pays the banker
+        //17. The banker then pays the player $1, and spins the wheel again.Suppose the number this time is 24. The
+        //banker pays another $1, and the process continues until a number is produced which has already occured.
+        //Suppose the sequence of numbers is 17,24,18,31. The banker has paid the player $4.00 . At the next turn of the
+        //wheel the number is 18 (a number already produced). At this point the game ends, and the player has lost
+        //$3.50. Simulate 200 such games, and display the number of times the
+        //player wins or loses, and the players total loss or gain.
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            LstView.Clear();
+            int wins = 0;
+            int losses = 0;
+            double total = 0;
+
+            for (int i = 1; i <= 200; i++)
+            {
+                int[] numbersPlayed = new int[37];
+                PlayTheRoulette(numbersPlayed, ref wins, ref losses, ref total);
+            }
+            LstView.Items.Add("Wins: "+ wins.ToString() + " Losses: " + losses.ToString() + " Total: " + total.ToString());
+        }
+
+        private void PlayTheRoulette(int[] NP, ref int w, ref int l, ref double t)
+        {
+            Random r = new Random();
+            double money = -7.50;
+            
+            //Check if there`s a previous number at the roulette
+            for(int x = 1; x <= 36; x++)
+            {
+                int spin = r.Next(1, 37);
+                NP[spin] += 1; //counter with number of times a number has been played
+                if (NP[spin] == 1)
+                {
+                    money += 1;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            t = money;
+
+            //Check if for every game the player has won more money than what he initially played (7.50$).
+            if (money > 0)
+            {
+                w += 1;
+            }
+            else
+            {
+                l += 1;
+            }
+        }
+    }
+}
